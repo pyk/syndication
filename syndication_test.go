464926976146b83354db459fcc515b75de71d15e
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2015-05-02 21:47:12 +0700

    add RSS 0.91 sample from specification site

--- syndication_test.go
+++ syndication_test.go
@@ -4,59 +4,59 @@ import (
 	"bytes"
 	"io"
 	"testing"
+	"time"
 )
 
-// source: http://cyber.law.harvard.edu/rss/examples/sampleRss091.xml
+// RSS 0.91 Spec http://www.rssboard.org/rss-0-9-1-netscape
 var RSS091 = `
-<?xml version="1.0" encoding="ISO-8859-1"?>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE rss SYSTEM "http://my.netscape.com/publish/formats/rss-0.91.dtd">
 <rss version="0.91">
-<channel>
-<title>WriteTheWeb</title> 
-<link>http://writetheweb.com</link> 
-<description>News for web users that write back</description> 
-<language>en-us</language> 
-<copyright>Copyright 2000, WriteTheWeb team.</copyright> 
-<managingEditor>editor@writetheweb.com</managingEditor> 
-<webMaster>webmaster@writetheweb.com</webMaster> 
-<image>
-<title>WriteTheWeb</title> 
-<url>http://writetheweb.com/images/mynetscape88.gif</url> 
-<link>http://writetheweb.com</link> 
-<width>88</width> 
-<height>31</height> 
-<description>News for web users that write back</description> 
-</image>
-<item>
-<title>Giving the world a pluggable Gnutella</title> 
-<link>http://writetheweb.com/read.php?item=24</link> 
-<description>WorldOS is a framework on which to build programs that work like Freenet or Gnutella -allowing distributed applications using peer-to-peer routing.</description> 
-</item>
-<item>
-<title>Syndication discussions hot up</title> 
-<link>http://writetheweb.com/read.php?item=23</link> 
-<description>After a period of dormancy, the Syndication mailing list has become active again, with contributions from leaders in traditional media and Web syndication.</description> 
-</item>
-<item>
-<title>Personal web server integrates file sharing and messaging</title> 
-<link>http://writetheweb.com/read.php?item=22</link> 
-<description>The Magi Project is an innovative project to create a combined personal web server and messaging system that enables the sharing and synchronization of information across desktop, laptop and palmtop devices.</description> 
-</item>
-<item>
-<title>Syndication and Metadata</title> 
-<link>http://writetheweb.com/read.php?item=21</link> 
-<description>RSS is probably the best known metadata format around. RDF is probably one of the least understood. In this essay, published on my O'Reilly Network weblog, I argue that the next generation of RSS should be based on RDF.</description> 
-</item>
-<item>
-<title>UK bloggers get organised</title> 
-<link>http://writetheweb.com/read.php?item=20</link> 
-<description>Looks like the weblogs scene is gathering pace beyond the shores of the US. There's now a UK-specific page on weblogs.com, and a mailing list at egroups.</description> 
-</item>
-<item>
-<title>Yournamehere.com more important than anything</title> 
-<link>http://writetheweb.com/read.php?item=19</link> 
-<description>Whatever you're publishing on the web, your site name is the most valuable asset you have, according to Carl Steadman.</description> 
-</item>
-</channel>
+    <channel>
+        <!-- required -->
+        <title>Title</title>
+        <description>Description</description>
+        <link>https://github.com/pyk/syndication</link>
+        <language>en-us</language>
+        <!-- optional -->
+        <copyright>Copyright 2015</copyright>
+        <docs>https://github.com/pyk/syndication</docs>
+        <image>
+            <url>https://github.com/pyk/profile.png</url>
+            <link>https://github.com/pyk</link>
+            <title>Image title</title>
+            <description>Image description</description>
+            <width>88</width>
+            <height>88</height>
+        </image>
+        <item>
+            <title>Item #1 title</title>
+            <link>https://github.com/pyk/syndciation</link>
+            <description>Item #1 description</description>
+        </item>
+        <item>
+            <title>Item #2 title</title>
+            <link>https://github.com/pyk/syndciation</link>
+            <description>Item #2 description</description>
+        </item>
+        <lastBuildDate>Sat, 02 May 2015 21:36:25 GMT</lastBuildDate>
+        <managingDirector>bayu@domain (Bayu)</managingDirector>
+        <pubDate>Sat, 02 May 2015 21:36:25 GMT</pubDate>
+        <rating>Rating</rating>
+        <skipDays>
+            <day>Sunday</day>
+        </skipDays>
+        <skipHours>
+            <hour>6</hour>
+        </skipHours>
+        <textInput>
+            <title>Search</title>
+            <description>Search description</description>
+            <name>Search</name>
+            <link>https://github.com/pyk/syndication</link>
+        </textInput>
+        <webMaster>bayu@domain (Bayu)</webMaster>
+    </channel>
 </rss>
 `
 
